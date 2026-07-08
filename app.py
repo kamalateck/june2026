4ed58650f7dc7d8f8fab667848476aea512e4a4f
qwikfeb-coder <anielrapole@gmail.com>
2026-07-08 11:01:21 +0530

    space

--- app.py
+++ app.py
@@ -8,3 +8,4 @@ def greet(name):
 
 if __name__ == "__main__":
     print(greet("GitHub"))
+    
--- test_app.py
+++ test_app.py
@@ -7,3 +7,4 @@ def test_add_numbers():
 
 def test_greet():
     assert greet("John") == "Hello, John"
+    
